5ae9c7395299b753541bf47cc800f15d8cfad956
rahul <rsbrmvm9@gmail.com>
2019-01-08 23:50:34 +0530

    11:50 PM

--- shortener/hosts.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-from django_hosts import patterns, host
-#from shortener.hostsconf import urls as redirect_urls
-
-host_patterns = patterns('',
-    host(r'myshortner4040.herokuapp.com', settings.ROOT_URLCONF, name='www'),
-   # host(r'(?!www).*', 'shortener.hostsconf.urls', name='wildcard'),
-)
--- url_shortner/settings/production.py
+++ url_shortner/settings/production.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django_hosts.middleware.HostsRequestMiddleware',
+    #'django_hosts.middleware.HostsRequestMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_hosts.middleware.HostsResponseMiddleware',
+    #'django_hosts.middleware.HostsResponseMiddleware',
 ]
 
 ROOT_URLCONF = 'url_shortner.urls'
