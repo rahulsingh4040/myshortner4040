d9de9c159a0d6085fe655a4d82f46096c4d595d1
rahul <rsbrmvm9@gmail.com>
2019-01-08 15:12:25 +0530

    Windows again

--- shortener/hosts.py
+++ shortener/hosts.py
@@ -3,6 +3,6 @@ from django_hosts import patterns, host
 #from shortener.hostsconf import urls as redirect_urls
 
 host_patterns = patterns('',
-    host(r'myshortner4040.herokuapp.com:8000', settings.ROOT_URLCONF, name='www'),
+    host(r'myshortner4040.herokuapp.com', settings.ROOT_URLCONF, name='www'),
    # host(r'(?!www).*', 'shortener.hostsconf.urls', name='wildcard'),
 )
